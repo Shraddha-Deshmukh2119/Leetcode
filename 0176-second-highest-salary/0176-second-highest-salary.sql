1bb3f57b00c5b26a28a761db4def3a9f40dca41b
Shraddha Bhivraj Deshmukh <shraddhabdeshmukh2110@gmail.com>
2025-07-23 21:48:27 +0530

    Time: 363 ms (9.77%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,9 @@
-SELECT 
-    (SELECT DISTINCT salary 
-     FROM Employee 
-     ORDER BY salary DESC 
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+-- SELECT 
+--     (SELECT DISTINCT salary 
+--      FROM Employee 
+--      ORDER BY salary DESC 
+--      LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+SELECT MAX(e1.salary) AS SecondHighestSalary
+FROM Employee e1 INNER JOIN Employee e2
+on e1.salary <e2.salary
